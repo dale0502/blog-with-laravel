9647333d6bb5d87f15bb21a344e546c5bf4c7c83
Dale <2100131110@nkust.edu.tw>
2021-02-20 09:58:32 +0800

    updata
    視圖優化及建立categories&posts table

---
+++ database/migrations/2021_02_20_094450_creat_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatCategoriesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('parent_id')->nullable()->unsigned()->index(); //父分類
+            $table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->string('title',100); //分類名稱
+            $table->string('pic',255)->nullable(); //分類圖片
+            $table->string('desc',500)->nullable(); //分類描述
+            $table->boolean('enabled')->default(true);//是否啟動
+            $table->integer('sort'); //排序
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('categories', function(Blueprint $table)
+        {
+            $table->dropForeign(['parent_id']);
+        });
+        Schema::dropIfExists('categories');
+    }
+}
